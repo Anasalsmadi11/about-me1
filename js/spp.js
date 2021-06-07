63380496fcc42beccfe664688482af317f7e30dc
Anasalsmadi11 <anas.alsmadi411@gmail.com>
2021-06-07 17:54:37 +0300

    guessing game

--- js/spp.js
+++ js/spp.js
@@ -11,13 +11,13 @@ if(myTravels === 'yes' || myTravels === 'y'){
 
   alert('Unfortunately, you got the wrong answer');
 
-  console.log('have I ever been to America befor?,what do you think?, no');
+  //console.log('have I ever been to America befor?,what do you think?, no');
 
 }else if(myTravels === 'no' || myTravels === 'n'){
 
   alert('good anser.');
 
-  console.log('have I ever been to America befor?,what do you think?, no');
+  //console.log('have I ever been to America befor?,what do you think?, no');
 }
 
 let myAge = prompt('do you think im above 20 or not?').toUpperCase();
@@ -26,13 +26,13 @@ if(myAge === 'YES' || myAge === 'Y'){
 
   alert('good answer , im already 27');
 
-  console.log('do you think im above 20 or not?,yes');
+  //console.log('do you think im above 20 or not?,yes');
 
 } else if( myAge === 'NO' || myAge === 'N'){
 
   alert('unfortunately, the answer is wrong');
 
-  console.log('do you think im above 20 or not?,yes');
+  //console.log('do you think im above 20 or not?,yes');
 }
 
 let myRisidence = prompt(' am I living by my self ?').toUpperCase();
@@ -41,13 +41,13 @@ if(myRisidence === 'YES' || myRisidence === 'Y'){
 
   alert('Unfortunately, you got the wrong answer');
 
-  console.log('am I living by my self ?, no');
+  //console.log('am I living by my self ?, no');
 
 } else if( myRisidence === 'NO' || myRisidence === 'N'){
 
   alert(' good answer ');
 
-  console.log('am I living by my self ?, no');
+  //console.log('am I living by my self ?, no');
 }
 
 let myHabit = prompt('do you think i like video games?').toLowerCase();
@@ -56,13 +56,13 @@ if( myHabit === 'yes' || myHabit === 'y'){
 
   alert('sorry , I dont like them');
 
-  console.log('do you think i like video games?,no');
+  //console.log('do you think i like video games?,no');
 
 } else if ( myHabit === 'no' || myHabit === 'n'){
 
   alert(' good answer');
 
-  console.log('do you think i like video games?,no');
+  //console.log('do you think i like video games?,no');
 }
 
 let myLicense = prompt('do you think i have a drive license?').toUpperCase();
@@ -71,13 +71,13 @@ if( myLicense === 'YES' || myLicense === 'Y'){
 
   alert(' wrong answer, i dont even know how to drive haha');
 
-  console.log('do you think i have a drive license?, no');
+  //console.log('do you think i have a drive license?, no');
 
 }else if ( myLicense === 'NO' || myLicense === 'N'){
 
   alert('nice anwer');
 
-  console.log('do you think i have a drive license?, no');
+  //console.log('do you think i have a drive license?, no');
 }
 
 alert(' I hope you enjoyed with this funny game :)');
